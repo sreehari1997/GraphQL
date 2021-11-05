dad6e72a357952f8b60fb61862c8d1da79b81056
sreehari1997 <sreeharivijayan619@gmail.com>
2021-11-05 12:39:47 +0530

    modelled Books, installed graphene

--- books/models.py
+++ books/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+
+class Book(models.Model):
+    title = models.CharField(max_length=100)
+    description = models.CharField(max_length=300)
+
+    def __str__(self):
+        return self.title
